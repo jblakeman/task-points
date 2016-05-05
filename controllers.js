7bcf50410ebc43abcd06d0c766ac9a16276aa62c
jblakeman <blakeman.john@gmail.com>
2016-05-05 19:18:23 -0400

    Add methods to task route handler

--- controllers.js
+++ controllers.js
@@ -1,4 +1,7 @@
 var logger = require("./logger");
+var models = require("./models");
+var Task   = models.Task;
+var Tag    = models.Tag;
 
 var sendFileOpts = {
     root: __dirname + "/public/",
@@ -22,6 +25,10 @@ var controllers = {
         });
     },
     taskCtrl: {
+        calcPoints: function(minutes, enjoyment) {
+            if (enjoyment > 1) { enjoyment *= 0.5; }
+            return Math.round(enjoyment * (1 + (minutes * 0.5)));
+        },
         index: function(req, res) {
             res.send({title: "Task Index"});
         },
@@ -30,7 +37,31 @@ var controllers = {
                 id: req.params.id,
                 title: "Task Show"
             });
+        },
+        create: function(req, res) {
+            var newTask = req.params.body;
+            Task.create(newTask).then(function(task) {
+                Task.update({
+                    points: calcPoints(newTask.estimated_time,
+                                       newTask.enjoyment_level)
+                }).then(function(task) {
+                    res.json(task);
+                }, function(err) {
+                    res.json(422, err);
+                });
+            });
+        },
+        update: function(req, res) {
+            Task.update(req.params.body).then(function(task) {
+                res.json(task);
+            });
+        },
+        destroy: function(req, res) {
+            Task.destroy(res.params.body).then(function() {
+                res.json({sucess: true});
+            });
         }
+
     }
 };
 
