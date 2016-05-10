08ed5553f98bd02ab9633ed054c2a74ff36f8506
jblakeman <blakeman.john@gmail.com>
2016-05-09 23:35:23 -0400

    Incorporate model queries
    * Reorder definitions
    * Render tree on show
    * Find all roots on index
    * Add handler for Task <-> Subtask association

--- controllers.js
+++ controllers.js
@@ -1,21 +1,25 @@
-var logger = require("./logger");
-var models = require("./models");
-var Task   = models.Task;
-var Tag    = models.Tag;
+function controllers(models, logger) {
+    var Task    = models.Task;
+    var Tag     = models.Tag;
+    var TaskSub = models.sequelize.models.TaskSub;
+    var TaskTag = models.sequelize.models.TaskTag;
 
-var sendFileOpts = {
-    root: __dirname + "/public/",
-    dotfiles: "deny",
-    headers: {
-        "x-timestamp": Date.now(),
-        "x-sent": true
-    }
-};
-
-var controllers = {
-    rootCtrl: function(req, res) {
+    var sendFileOpts = {
+        root: __dirname + "/public/",
+        dotfiles: "deny",
+        headers: {
+            "x-timestamp": Date.now(),
+            "x-sent": true
+        }
+    };
+    var getPlain = function(instance) {
+        return instance.get({plain: true});
+    };
+    var ctrls = {};
+    ctrls.rootCtrl = function(req, res) {
         var fileName = "index.html";
-        logger.info("Responding with " + fileName + " to root get request");
+        logger.info("Responding with " + fileName +
+                    " to root get request");
         res.sendFile(fileName, sendFileOpts, function(err) {
             if (err) {
                 logger.error(err);
@@ -23,46 +27,112 @@ var controllers = {
                 logger.info("Sent: " + fileName);
             }
         });
-    },
-    taskCtrl: {
-        calcPoints: function(minutes, enjoyment) {
-            if (enjoyment > 1) { enjoyment *= 0.5; }
-            return Math.round(enjoyment * (1 + (minutes * 0.5)));
+    };
+    ctrls.taskCtrl = {
+        setPoints: function(task) {
+            if (task.enjoyment > 1) { task.enjoyment *= 0.5; }
+            task.points = Math.round(task.enjoyment *
+                                     (1 + (task.minutes * 0.5)));
+            return task;
         },
         index: function(req, res) {
-            res.send({title: "Task Index"});
+            // Sequelize doesn't support self associations very well
+            // so we do some hackery to find all root tasks (projects)
+            var subIds = [];
+            logger.info("Building task index...");
+            return TaskSub.findAll({}).then(function(tasksubs) {
+                logger.info("TaskSubs retrieved...");
+                tasksubs.forEach(function(tasksub) {
+                    subIds.push(getPlain(tasksub).SubtaskId);
+                });
+            }).then(function() {
+                logger.info("Finding Tasks that aren't Subtasks (roots)");
+                Task.findAll({where: {id: {$notIn: subIds}}})
+                    .then(function(parents) {
+                        res.json(parents);
+                    }, function(err) {
+                        logger.error("Unable to retrieve root tasks:", err);
+                        res.send(400, err);
+                    });
+            });
         },
         show: function(req, res) {
-            res.send({
-                id: req.params.id,
-                title: "Task Show"
+            // Depth-first search to find all Subtasks
+            // of projects, and their children.
+            //
+            // TODO: Replace this with a raw, postgres CTE query.
+            function traverse(nodes) {
+                // Start by querying for children (pre-order)
+                return node.getSubtasks().then(function(subs) {
+                    return {task: node, children: subs};
+                }).then(function(t) {
+                    var inorders = [];
+                    if (t.children.length > 0) {
+                        // Traverse all the subtasks if they exist (i-N in-order)
+                        t.children.forEach(function(child, i) {
+                            orders.push(traverse(child).then(function(children) {
+                                t.children[i] = children;
+                                return true;
+                            }));
+                        });
+                    }
+                    // completes a node traversal (post-order)
+                    return Promise.all(orders).then(function() {
+                        return t;
+                    });
+                });
+            }
+            Task.findById(req.params.id).then(function(task) {
+                // Build root task's generic tree using depth-first search
+                traverse(task).then(function(tree) {
+                    res.json(tree);
+                });
+            }, function(err) {
+                res.send(400, err);
             });
         },
         create: function(req, res) {
-            var newTask = req.params.body;
-            Task.create(newTask).then(function(task) {
-                Task.update({
-                    points: calcPoints(newTask.estimated_time,
-                                       newTask.enjoyment_level)
-                }).then(function(task) {
-                    res.json(task);
-                }, function(err) {
-                    res.json(422, err);
-                });
+            Task.create(this.setPoints(req.body)).then(function(task) {
+                res.json(task);
+            }, function(err) {
+                res.send(400, err);
+                throw err;
             });
         },
         update: function(req, res) {
-            Task.update(req.params.body).then(function(task) {
+            Task.update(req.body).then(function(task) {
                 res.json(task);
             });
         },
-        destroy: function(req, res) {
-            Task.destroy(res.params.body).then(function() {
-                res.json({sucess: true});
+        destroy: function(req, res, isHelper) {
+            // Sequelize removes all relationships attached to columns
+            // when they're destroyed by default (CASCADE).
+            Task.findById(req.params.id).then(function(task) {
+                task.destroy().then(function() {
+                    res.json({sucess: true});
+                }, function(err) {
+                    res.send(400, err);
+                });
+            }, function(err) {
+                res.send(400, err);
+            });
+        },
+        addSub: function(req, res) {
+            Task.create(this.setPoints(req.body)).then(function(task) {
+                Task.findById(req.params.id).then(function(parentTask) {
+                    parentTask.addSubtask(task).then(function() {
+                        res.json(task);
+                    }, function(err) {
+                        res.send(400, err);
+                    });
+                }, function(err) {
+                    res.send(400, err);
+                });
+            }, function(err) {
+                res.send(400, err);
             });
         }
+    };
+}
 
-    }
-};
-
-module.exports = controllers;
+module.exports = controllers(model, logger).ctrls;
