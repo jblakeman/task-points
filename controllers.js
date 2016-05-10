945ece3b2bcf6dbdd9e4f86682ece5b63697ebf6
jblakeman <blakeman.john@gmail.com>
2016-05-10 14:34:14 -0400

    More logging

--- controllers.js
+++ controllers.js
@@ -82,53 +82,78 @@ function controllers(models, logger) {
                     });
                 });
             }
+            logger.info("Task show reached, searching for task.id:",
+                        req.params.id, "...");
             Task.findById(req.params.id).then(function(task) {
                 // Build root task's generic tree using depth-first search
+                logger.info("Task found. Traversing children...");
                 traverse(task).then(function(tree) {
+                    logger.info("Task tree successfully traversed");
                     res.json(tree);
                 });
             }, function(err) {
+                logger.error("Failed to find task:", err);
                 res.send(400, err);
             });
         },
         create: function(req, res) {
+            logger.info("Creating new task...");
             Task.create(this.setPoints(req.body)).then(function(task) {
+                logger.info("Task successfully created, id:", getPlain(task).id);
                 res.json(task);
             }, function(err) {
+                logger.error("Task creation failed:", err);
                 res.send(400, err);
                 throw err;
             });
         },
         update: function(req, res) {
+            logger.info("Updating task:", req.params.id, "...");
             Task.update(req.body).then(function(task) {
+                logger.info("Task successfully updated");
                 res.json(task);
+            }, function(err) {
+                logger.error("Task update failed:", err);
+                res.send(400, err);
             });
         },
         destroy: function(req, res, isHelper) {
             // Sequelize removes all relationships attached to columns
             // when they're destroyed by default (CASCADE).
+            logger.info("Destroying task with id:", req.params.id);
             Task.findById(req.params.id).then(function(task) {
+                logger.info("Task found, destroying...");
                 task.destroy().then(function() {
+                    logger.info("Task successfully destroyed");
                     res.json({sucess: true});
                 }, function(err) {
+                    logger.error("Task could not be destroyed:", err);
                     res.send(400, err);
                 });
             }, function(err) {
+                logger.error("Unable to find task:", err);
                 res.send(400, err);
             });
         },
         addSub: function(req, res) {
+            logger.info("Adding Subtask to task:", req.params.id, "...");
             Task.create(this.setPoints(req.body)).then(function(task) {
+                logger.info("Task successfully created with id:", getPlain(task).id);
                 Task.findById(req.params.id).then(function(parentTask) {
                     parentTask.addSubtask(task).then(function() {
+                        logger.info("Task:", getPlain(task).id,
+                                    "successfully added as subtask");
                         res.json(task);
                     }, function(err) {
+                        logger.error("Unable to add as subtask:", err);
                         res.send(400, err);
                     });
                 }, function(err) {
+                    logger.error("Unable to find parent task:", err);
                     res.send(400, err);
                 });
             }, function(err) {
+                logger.error("Unable to create new task:", err);
                 res.send(400, err);
             });
         }
